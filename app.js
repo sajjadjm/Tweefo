918f94299e1ecd3e2eccea0d1ae3e6e8be3fff9e
sajjadjm <msajjad.jm@gmail.com>
2020-07-29 19:52:40 +0430

    now can get followers and followings

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', exphbs({defaultLayout: ''}));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const admin = require('./routes/admin-routes');
 
--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -4,13 +4,45 @@ const tweefo = require('../axios/twitter-requests');
 
 router.get('/', async (req, res) => {
 
-    const token = await GetToken();
-
-    const result = await GetFollowings('pouya__ip', '-1', token);
-
-    console.log(result);
-
     res.render('index');
+
+});
+
+router.post('/get_info', async (req, res) => {
+
+    console.log(req.body);
+
+    const token = await GetToken();
+    // const result = await GetFollowings('pouya__ip', '-1', token);
+
+    let users = new Array();
+    
+    if(req.body.rdo == '1')
+    {
+        let cursor = -1;
+        
+        while(cursor != 0)
+        {
+            const result = await GetFollowers(req.body.name, cursor, token);
+            users = users.concat(result.users);
+            cursor = result.next_cursor;
+        }
+    }
+    else if(req.body.rdo == '2')
+    {
+        let cursor = -1;
+        
+        while(cursor != 0)
+        {
+            const result = await GetFollowings(req.body.name, cursor, token);
+            users = users.concat(result.users);
+            cursor = result.next_cursor;
+        }
+    }
+
+    users.forEach(user => {
+        console.log(user.screen_name);
+    });
 
 });
 
